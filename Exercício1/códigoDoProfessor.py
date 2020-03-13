f8ab1355f29c7157000fe5aadaa5273a01d6cff7
Pedro Ian <pedroianmoraes@gmail.com>
2020-03-13 14:15:55 -0300

    test file

--- Exercício1/códigoDoProfessor.py
+++ Exercício1/códigoDoProfessor.py
@@ -1,13 +1,13 @@
 import numpy as np
 #import seaborn as sns
 from scipy.stats import norm
+from scipy.stats import kde
 from matplotlib import pyplot as plt
-
 
 from sklearn.neighbors import KernelDensity
 
 origin = 0
-var = 0.6 # Variancia da Distribuicao Normal
+var = 0.36  # Variancia da Distribuicao Normal
 
 # Vetores das posicoes das amostras 1 e 2
 x1 = []
@@ -26,15 +26,34 @@ for i in range(100):
     y2 += [np.random.normal(origin, var) + 4]
     X2 += [x2, y2]
 
-plt.plot(x1, y1, 'r.', x2, y2, 'b+') #  '( r | g | b )'  +  '( o | -- | ^ | . )'
+
+nbins = 100
+k = kde.gaussian_kde([x1, y1])
+xi, yi = np.mgrid[0:6:nbins*1j, 0:6:nbins*1j]
+zi = k(np.vstack([xi.flatten(), yi.flatten()]))
+print(zi)
+
+plt.contour(x1, y1, zi)
+
+k2 = kde.gaussian_kde([x2, y2])
+zii = k2(np.vstack([xi.flatten(), yi.flatten()]))
+
+plt.pcolormesh(xi, yi, zi.reshape(xi.shape))
+plt.pcolormesh(xi, yi, zii.reshape(xi.shape))
+plt.show()
+
+# '( r | g | b )'  +  '( o | -- | ^ | . )'
+plt.plot(x1, y1, 'r.', x2, y2, 'b+')
 plt.axis([0, 6, 0, 6])
 plt.show()
 
-base = np.linspace(0,6,100)
+# base = np.linspace(0, 6, 100)
+# z = kde(X1)(X1)
+# plt.plot(z)
 
-kde = KernelDensity(bandwidth=0.2).fit(X1, 2)
-print(len(kde.sample()[0]))
-plt.plot(np.transpose(base), kde.sample())
-plt.show()
+# kde = KernelDensity(bandwidth=0.2).fit(X1, 2)
+# print(len(kde.sample()[0]))
+# plt.plot(np.transpose(base), kde.sample())
+# plt.show()
 
-#sns.distplot(x1)
+# sns.distplot(x1)
---
+++ Exercício1/teste.py
@@ -0,0 +1,43 @@
+# # libraries
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from scipy.stats import kde
+
+# # create data
+# x = np.random.normal(size=500)
+# y = x * 3 + np.random.normal(size=500)
+
+# # Evaluate a gaussian kde on a regular grid of nbins x nbins over data extents
+# nbins = 300
+# k = kde.gaussian_kde([x, y])
+# xi, yi = np.mgrid[x.min():x.max():nbins*1j, y.min():y.max():nbins*1j]
+# zi = k(np.vstack([xi.flatten(), yi.flatten()]))
+
+# plt.scatter(x, y)
+# plt.show()
+
+# # Make the plot
+# plt.pcolormesh(xi, yi, zi.reshape(xi.shape))
+# plt.show()
+
+# # Change color palette
+# # plt.pcolormesh(xi, yi, zi.reshape(xi.shape), cmap=plt.cm.Greens_r)
+# # plt.show()
+
+
+ 
+# library & dataset
+import seaborn as sns
+df = sns.load_dataset('iris')
+ 
+# Basic 2D density plot
+sns.set_style("white")
+sns.kdeplot(df.sepal_width, df.sepal_length)
+#sns.plt.show()
+ 
+# Custom it with the same argument as 1D density plot
+sns.kdeplot(df.sepal_width, df.sepal_length, cmap="Reds", shade=True, bw=.15)
+ 
+# Some features are characteristic of 2D: color palette and wether or not color the lowest range
+sns.kdeplot(df.sepal_width, df.sepal_length, cmap="Blues", shade=True, shade_lowest=True, )
+sns.plt.show()
