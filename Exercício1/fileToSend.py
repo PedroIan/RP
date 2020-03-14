67fd96c70909668b57131c1f8b3e86024ea7ec61
Pedro Ian <pedroianmoraes@gmail.com>
2020-03-13 21:48:55 -0300

    sent that way

--- Exercício1/fileToSend.py
+++ Exercício1/fileToSend.py
@@ -4,6 +4,28 @@ from scipy.stats import norm
 from scipy.stats import kde
 from matplotlib import pyplot as plt
 
+def pdfUnivariada(sigma, media, inputX):
+    expoente = - np.square(inputX - media)/(2 * np.square(sigma))
+    return np.e ** expoente / (sigma * (np.sqrt(2 * np.pi)))
+
+def calculoVerossimilhanca(sigma1, sigma2, media1, media2, inputX, inputY, coeficienteCorrelacao=0):
+    expoente = -(np.square((inputX - media1)/sigma1) + np.square((inputY - media2)/sigma2) - 2*coeficienteCorrelacao *
+                ((inputX - media1)/sigma1)*((inputY - media2)/sigma2))/(2*(1 - np.square(coeficienteCorrelacao)))
+    return (np.e**expoente)/(2*np.pi*sigma1*sigma2*np.sqrt(1 - np.square(coeficienteCorrelacao)))
+
+
+def media(input):
+    return sum(input)/len(input)
+
+
+def desvioPadrao(input):
+    med = media(input)
+    somatorio = 0
+    for i in input:
+        somatorio += np.square(i - med)
+    return np.sqrt(somatorio/len(input))
+
+
 origin = 0
 var = 0.36  # Variancia da Distribuicao Normal
 var2 = 0.6
@@ -18,10 +40,71 @@ y2 = []
 X2 = []
 
 for i in range(100):
-    x1 += [np.random.normal(origin, var2) + 2]
-    y1 += [np.random.normal(origin, var2) + 2]
+    x1 += [np.random.normal(origin, var) + 2]
+    y1 += [np.random.normal(origin, var) + 2]
     x2 += [np.random.normal(origin, var) + 4]
     y2 += [np.random.normal(origin, var) + 4]
+
+mediaX1 = media(x1)
+mediaY1 = media(y1)
+desvioX1 = desvioPadrao(x1)
+desvioY1 = desvioPadrao(y1)
+
+mediaX2 = media(x2)
+mediaY2 = media(y2)
+desvioX2 = desvioPadrao(x2)
+desvioY2 = desvioPadrao(y2)
+
+result = []
+
+xAxesBase = np.linspace(0, 6, 100)
+
+distribuicaoX1 = []
+distribuicaoY1 = []
+distribuicaoX2 = []
+distribuicaoY2 = []
+
+for i in range(len(x1)):
+    distribuicaoX1 += [pdfUnivariada(desvioX1, mediaX1, x1[i])]
+for i in range(len(y1)):
+    distribuicaoY1 += [pdfUnivariada(desvioY1, mediaY1, y1[i])]
+for i in range(len(x2)):
+    distribuicaoX2 += [pdfUnivariada(desvioX2, mediaX2, x2[i])]
+for i in range(len(y2)):
+    distribuicaoY2 += [pdfUnivariada(desvioY2, mediaY2, y2[i])]
+
+plt.scatter(x1, distribuicaoX1)
+plt.scatter(y1, distribuicaoY1)
+plt.scatter(x2, distribuicaoX2)
+plt.scatter(y2, distribuicaoY2)
+plt.show()
+
+testeResult = []
+for i in range(len(distribuicaoX1)):
+    testeResult.append([distribuicaoX1[i], distribuicaoY1[i]])
+
+for i in x1:
+    temp = []
+    for j in y1:
+        temp += [calculoVerossimilhanca(float(desvioX1), float(desvioY1), float(
+            mediaX1), float(mediaY1), float(i), float(j), 0)]
+    result += [temp]
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+xMesh, yMesh = np.meshgrid(distribuicaoX1, distribuicaoY1)
+ax.plot_surface(x1, y1, np.asarray(result))
+plt.show()
+
+mediaX2 = media(x2)
+mediaY2 = media(y2)
+desvioX2 = desvioPadrao(x2)
+desvioY2 = desvioPadrao(y2)
+
+plt.scatter(x1, y1)
+plt.scatter(x2, y2)
+plt.show()
+
 
 something = sns.kdeplot(x1, y1)
 something2 = sns.kdeplot(x2, y2)
--- Exercício1/teste.py
+++ Exercício1/teste.py
@@ -26,19 +26,51 @@
 
 
  
-# library & dataset
-import seaborn as sns
-from matplotlib import pyplot as plt
-df = sns.load_dataset('iris')
+# # library & dataset
+# import seaborn as sns
+# from matplotlib import pyplot as plt
+# df = sns.load_dataset('iris')
  
-# Basic 2D density plot
-sns.set_style("white")
-sns.kdeplot(df.sepal_width, df.sepal_length)
-plt.show()
+# # Basic 2D density plot
+# sns.set_style("white")
+# sns.kdeplot(df.sepal_width, df.sepal_length)
+# plt.show()
  
-# Custom it with the same argument as 1D density plot
-sns.kdeplot(df.sepal_width, df.sepal_length, cmap="Reds", shade=True, bw=.15)
+# # Custom it with the same argument as 1D density plot
+# sns.kdeplot(df.sepal_width, df.sepal_length, cmap="Reds", shade=True, bw=.15)
  
-# Some features are characteristic of 2D: color palette and wether or not color the lowest range
-sns.kdeplot(df.sepal_width, df.sepal_length, cmap="Blues", shade=True, shade_lowest=True, )
+# # Some features are characteristic of 2D: color palette and wether or not color the lowest range
+# sns.kdeplot(df.sepal_width, df.sepal_length, cmap="Blues", shade=True, shade_lowest=True, )
+# plt.show()
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# Make data.
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+print(Z)
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
 plt.show()
