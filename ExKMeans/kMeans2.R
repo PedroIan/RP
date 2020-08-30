0dc5a3449438e5388afa737ea76efb2a45d82882
Pedro Ian <pedroianmoraes@gmail.com>
2020-08-30 15:58:48 -0300

    update working-sh

--- ExKMeans/kMeans2.R
+++ ExKMeans/kMeans2.R
@@ -1,5 +1,9 @@
 library('mlbench')
 source('kMeans.R')
+library('plot3D')
+library('rgl')
+
+library(MASS)
 
 p<-mlbench.spirals(1000,cycles=1, sd=0.05)
 x <- p[[1]]
@@ -18,10 +22,29 @@ pontosClusterizados <- retList[[2]]
 
 cores <- rainbow(nClusters)
 
+den3d <- kde2d(pontosClusterizados[,1],pontosClusterizados[,2], h=1, lims=c(-1.5,1.5,-1.5,1.5))
+
+
+xResult <- den3d$x
+yResult <- den3d$y
+zResult <- matrix(0, nrow = nrow(den3d$z), ncol = ncol(den3d$z))
+
 for(i in 1:nClusters){
+  
   pointsToPlot <- pontosClusterizados[pontosClusterizados[,3] == i,]
-  plot(pointsToPlot[,1],pointsToPlot[,2],col = cores[i], xlim = c(-1.5,1.5), ylim = c(-1.5,1.5), xlab='x', ylab='y')
-  par(new=T)
+  
+  if(nrow(pointsToPlot) > 0){
+    plot(pointsToPlot[,1],pointsToPlot[,2],col = cores[i], xlim = c(-1.5,1.5), ylim = c(-1.5,1.5), xlab='x', ylab='y')
+    par(new=T)
+    den3d <- kde2d(pointsToPlot[,1],pointsToPlot[,2], lims=c(-1.5,1.5,-1.5,1.5))
+    
+    #persp3D(xResult, yResult, den3d$z, counter=T, theta = 55, phi = 80, r = 40, d = 0.1, expand = 0.5, ltheta = 90, lphi = 180, shade = 0.4, ticktype = 'detailed', nticks=5)
+    
+    zResult <- zResult + den3d$z
+    
+  }
 }
 
+persp3D(xResult, yResult, zResult, counter=T, theta = 55, phi = 85, r = 10000000000, d = 0.000001, expand = 2, ltheta = 90, lphi = 90, shade = 0.4, ticktype = 'detailed', nticks=5)
+#movie3d(spin3d(axis = c(0,0,1), rpm = 10), duration=6,  type = "png")
 #plot(p)
