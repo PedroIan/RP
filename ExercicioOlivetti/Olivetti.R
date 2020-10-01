ae7c913dee4853d1a3db24fd0480c9ae6c280c7b
Pedro Ian <pedroianmoraes@gmail.com>
2020-10-01 19:41:33 -0300

    resultados

--- ExercicioOlivetti/Olivetti.R
+++ ExercicioOlivetti/Olivetti.R
@@ -1,38 +1,79 @@
+
 rm(list = ls())
 require(RnavGraphImageData)
 source('meuPCA.R')
 library(ggfortify)
+require(caret)
+require(ks)
 library(mlbench)
+library(penaltyLearning)
+library(e1071)
 
 data(faces)
 faces <- t(faces)
-
-a<-mlbench.2dnormals(200)
-
-MostraImagem <- function( x ) {
-  rotate <- function(x) t( apply(x, 2, rev) )
-  img <- matrix( x, nrow=64 )
-  cor <- rev( gray(50:1/50) )
-  image( rotate( img ), col=cor )
-}
+cores <-rainbow(50)
 
 dimensaoFinal<-2
-
-points <- matrix(0, nrow=nrow(faces), ncol=dimensaoFinal)
-for (i in 1:nrow(faces)){
-  
-  MostraImagem( faces[i,])
-  
-  singleImage <- matrix( faces[i,], nrow=64 )
-  Sys.sleep(3)
-  #pcaResult <- meuPCA(singleImage, dimensaoFinal)
-  #points[i,] <- singleImage %*% pcaResult[[2]]
-  
-}
 
 rotulos <- NULL
 for(i in 1:nrow(faces) ) {
   rotulos <- c( rotulos, ((i-1) %/% 10) + 1 )
 }
+
+#for (i in 1:nrow(faces)){
+#  MostraImagem( faces[i,])
+#}
+
+points <- matrix(0, nrow=nrow(faces), ncol=dimensaoFinal)
+for (i in 1:nrow(faces)){
+  
+  singleImage <- matrix( faces[i,], nrow=64 )
+  pcaResult <- meuPCA(singleImage, dimensaoFinal)
+  
+  
+  newData <- singleImage %*% pcaResult[[2]]
+  newData[is.na(newData)] <- 0
+  points[i,] <- colSums(newData, na.rm = TRUE) / nrow(newData)
+  
+}
+
+for(i in 1:ncol(points)) {
+  points[,i] <- points[,i] - mean(points[,i])
+}
+
+newConfusionMatrix <- matrix(0, ncol = 40, nrow= 40)
+acuracia <- NULL
+
+for(i in 1:10) {
+  trainIndex <- createDataPartition(points[,1], p=0.5, list=FALSE)
+  
+  #trainCopy <- data.frame(par = points[trainIndex,], class = factor(rotulos[trainIndex]))
+  #trainCopy[is.na(trainCopy)] <- 0
+  
+  trainCopy <- data.frame(par = faces[trainIndex,], class = factor(rotulos[trainIndex]))
+  
+  #testDf <- data.frame(par = points[-trainIndex,], class = factor(rotulos[-trainIndex]))
+  #testDf[is.na(testDf)] <- 0
+  
+  testDf <- data.frame(par = faces[-trainIndex,], class = factor(rotulos[-trainIndex]))
+  
+  
+  modelNB <- naiveBayes(class ~ ., data = trainCopy, laplace = 0, na.action = na.pass)
+  
+  predictions <- predict(modelNB, testDf)
+  cMatrix <- confusionMatrix(predictions, testDf$class)
+  newConfusionMatrix <- newConfusionMatrix + cMatrix$table
+  
+  acuracia <- c( acuracia, as.numeric(cMatrix$overall[[1]]) )
+}
+
+mean(acuracia)
+sd(acuracia)
+
+write.csv(newConfusionMatrix, file = "confusionMatrix.csv")
+print('Matriz salva!')
+
+indexesAll <- c(1:10,41:50,81:90,121:130)
+plot(abs(points[,1]), abs(points[,2]), col=cores[rotulos])
 
 
--- ExercicioOlivetti/meuPCA.R
+++ ExercicioOlivetti/meuPCA.R
@@ -12,9 +12,22 @@ meuPCA <- function( xIn, nDimensoesFinal ) {
   randomColumns <- sample.int(ncol(decomposicao$vectors), nDimensoesFinal)
   vectors <- matrix(0, ncol=nDimensoesFinal, nrow = nrow(decomposicao$vectors))
   
-  for (i in 1:length(randomColumns)) {
+  sorted <- sort(decomposicao$values, decreasing = TRUE)
+  
+  index1 <- match(sorted[1], decomposicao$values) 
+  index2 <- match(sorted[2], decomposicao$values) 
+  
+  for (i in c(index1, index2)) {
     vectors[,i] <- decomposicao$vectors[,i]
   }
   
+  
   return(list(decomposicao$values, vectors))
+}
+
+MostraImagem <- function( x ) {
+  rotate <- function(x) t( apply(x, 2, rev) )
+  img <- matrix( x, nrow=64 )
+  cor <- rev( gray(50:1/50) )
+  image( rotate( img ), col=cor )
 }
