698106dc3c403926a8a90d684f73cdbb222dec75
Pedro Ian <pedroianmoraes@gmail.com>
2020-10-31 02:37:57 -0300

    Update TrabalhoFinal.R

--- TrabalhoFinal/TrabalhoFinal.R
+++ TrabalhoFinal/TrabalhoFinal.R
@@ -6,6 +6,9 @@ library(SparseM)
 require(caret)
 require(ks)
 
+###########################################
+# Adquirindo dados
+
 table = read.csv('EURUSD_D1.csv')
 
 dadosOpen = table$Open
@@ -17,6 +20,9 @@ mediaData = rowMeans(allData)
 
 dadosEmMatriz = matrix(0, nrow = (length(mediaData) - 101), ncol = 101)
 
+###########################################
+# Transformando os dados em matrizes
+
 for (i in 1:(length(mediaData) - 101)) {
   dadosEmMatriz[i,] = mediaData[i:(i + 100)]
   
@@ -27,6 +33,9 @@ for (i in 1:(length(mediaData) - 101)) {
   }
 }
 
+###########################################
+# Separando dados de treinamento e testes
+
 trainIndex <- createDataPartition(c(1:(length(mediaData)-101)), p=0.7, list=FALSE)
 
 dadosTreinamento = dadosEmMatriz[trainIndex,]
@@ -34,12 +43,23 @@ dadosTeste = dadosEmMatriz[-trainIndex,]
 
 dataFrame = data.frame(dadosTreinamento[,1:100], y = as.factor(dadosTreinamento[,101]))
 
-SVMModel = svm(y ~ ., data = dataFrame, kernel = "radial", cost = 100, scale = FALSE)
+###########################################
+# Modelo arbitrário
 
-tune.out=tune(svm ,y~.,data=dataFrame ,kernel ="radial", 
-              ranges =list(cost=c(0.001,0.01,0.1, 1,5,10,100)))
-summary(tune.out)
+SVMModel = svm(y ~ ., data = dataFrame, kernel = "linear", cost = 100, scale = FALSE)
 
 predictions = predict(SVMModel,dadosTeste[,1:100])
-
 confusionMatrix(factor(predictions), factor(dadosTeste[,101]))
+
+###########################################
+# Método de validação cruzada
+
+tune.out=tune(svm ,y~.,data=dataFrame ,kernel ="radial", 
+              ranges =list(cost=c(10000)))
+
+tune.out=tune(svm ,y~.,data=dataFrame ,kernel ="linear", 
+              ranges =list(cost=c(2000)))
+summary(tune.out)
+
+predictionsTune = predict(tune.out$best.model,dadosTeste[,1:100])
+confusionMatrix(factor(predictionsTune), factor(dadosTeste[,101]))
